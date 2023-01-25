2b5a46aa5d739b86edd9e0558c7b0d4562441642
Mukul <mukul1singh23@gmail.com>
2023-01-25 23:28:18 +0530

    typeit

--- typeit.java
+++ typeit.java
@@ -14,11 +14,11 @@ public class typeit {
                 t2=s.substring(i+1,(i+1+t1.length()));
             else
                 t2=s.substring(i+1);
-            System.out.println("t1= "+t1+" t2= "+t2);
+            // System.out.println("t1= "+t1+" t2= "+t2);
             if(t1.equals(t2))
             {
                 n=Math.min(n, i+2+(s.length()-(t2.length()*2)));
-                System.out.println(n);
+                // System.out.println(n);
             }
         }
         n=Math.min(n, s.length());
